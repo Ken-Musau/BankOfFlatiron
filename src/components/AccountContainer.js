6720ee979fc40535b093ab61d1f8b75d3724afb4
Kennedy Musau <kennedy.musauu@gmail.com>
2023-06-05 21:42:01 +0300

    transferd logic from app component

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,17 +1,118 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import AddTransactionForm from "./AddTransactionForm";
 
-function AccountContainer({
-  transactions,
-  changeHandler,
-  submitHandler,
-  searchHandler,
-  deleteHandler,
-  sortByCategory,
-  sortByDescription,
-  formData,
-}) {
+function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [originalTransactions, setOriginalTransactions] = useState([]); // return original list of transaction when searchquery is cleared
+  const [formData, setFormData] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: "",
+  });
+
+  const PostFormData = {
+    date: formData.date,
+    description: formData.description,
+    category: formData.category,
+    amount: formData.amount,
+  };
+
+  const base_url = "http://localhost:8001/transactions";
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  async function fetchTransactions() {
+    try {
+      const res = await fetch(base_url);
+      const transactionsFetched = await res.json();
+      setTransactions(transactionsFetched);
+      setOriginalTransactions(transactionsFetched);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function postTransaction() {
+    try {
+      const res = await fetch(base_url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(PostFormData),
+      });
+      const postedTransaction = await res.json();
+      setTransactions([...transactions, postedTransaction]);
+      setOriginalTransactions([...originalTransactions, postedTransaction]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function deleteTransaction(id) {
+    try {
+      await fetch(`${base_url}/${id}`, {
+        method: "DELETE",
+      });
+      setTransactions(
+        transactions.filter((transaction) => transaction.id !== id)
+      );
+      setOriginalTransactions(
+        originalTransactions.filter((transaction) => transaction.id !== id)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Keep truck of changes in transaction inputs
+  const onChangeHandler = (e) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSearchHandler = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const filteredTransactions = originalTransactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm)
+    );
+    setTransactions(searchTerm ? filteredTransactions : originalTransactions);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    postTransaction();
+    setFormData({
+      date: "",
+      description: "",
+      category: "",
+      amount: "",
+    });
+  };
+
+  const onDeleteHandler = (id) => {
+    deleteTransaction(id);
+    console.log("Deleted", id);
+  };
+
+  const sortTransactionsByCategoryHandler = () => {
+    const sortedTransactions = [...transactions].sort((a, b) =>
+      a.category.localeCompare(b.category)
+    );
+    setTransactions(sortedTransactions);
+  };
+
+  const sortTransactionsByDescriptionHandler = () => {
+    const sortedTransactions = [...transactions].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+    setTransactions(sortedTransactions);
+  };
+
   return (
     <div>
       <div className="ui segment">
@@ -19,21 +120,21 @@ function AccountContainer({
           <input
             type="text"
             placeholder="Search your Recent Transactions"
-            onChange={searchHandler}
+            onChange={onSearchHandler}
           />
           <i className="circular search link icon"></i>
         </div>
       </div>
       <AddTransactionForm
-        changeHandler={changeHandler}
-        submitHandler={submitHandler}
+        changeHandler={onChangeHandler}
+        submitHandler={onSubmitHandler}
         formData={formData}
       />
       <TransactionsList
         transactions={transactions}
-        deleteHandler={deleteHandler}
-        sortByCategory={sortByCategory}
-        sortByDescription={sortByDescription}
+        deleteHandler={onDeleteHandler}
+        sortByCategory={sortTransactionsByCategoryHandler}
+        sortByDescription={sortTransactionsByDescriptionHandler}
       />
     </div>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -1,134 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AccountContainer from "./AccountContainer";
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
-  const [originalTransactions, setOriginalTransactions] = useState([]); // return original list of transaction when searchquery is cleared
-  const [formData, setFormData] = useState({
-    date: "",
-    description: "",
-    category: "",
-    amount: "",
-  });
-
-  const PostFormData = {
-    date: formData.date,
-    description: formData.description,
-    category: formData.category,
-    amount: formData.amount,
-  };
-
-  const base_url = "http://localhost:8001/transactions";
-
-  useEffect(() => {
-    fetchTransactions();
-  }, []);
-
-  async function fetchTransactions() {
-    try {
-      const res = await fetch(base_url);
-      const transactionsFetched = await res.json();
-      setTransactions(transactionsFetched);
-      setOriginalTransactions(transactionsFetched);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async function postTransaction() {
-    try {
-      const res = await fetch(base_url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(PostFormData),
-      });
-      const postedTransaction = await res.json();
-      setTransactions([...transactions, postedTransaction]);
-      setOriginalTransactions([...originalTransactions, postedTransaction]);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async function deleteTransaction(id) {
-    try {
-      await fetch(`${base_url}/${id}`, {
-        method: "DELETE",
-      });
-      setTransactions(
-        transactions.filter((transaction) => transaction.id !== id)
-      );
-      setOriginalTransactions(
-        originalTransactions.filter((transaction) => transaction.id !== id)
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  // Keep truck of changes in transaction inputs
-  const onChangeHandler = (e) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const onSearchHandler = (e) => {
-    const searchTerm = e.target.value.toLowerCase();
-    const filteredTransactions = originalTransactions.filter((transaction) =>
-      transaction.description.toLowerCase().includes(searchTerm)
-    );
-    setTransactions(searchTerm ? filteredTransactions : originalTransactions);
-  };
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    postTransaction();
-    setFormData({
-      date: "",
-      description: "",
-      category: "",
-      amount: "",
-    });
-  };
-
-  const onDeleteHandler = (id) => {
-    deleteTransaction(id);
-    console.log("Deleted", id);
-  };
-
-  const sortTransactionsByCategoryHandler = () => {
-    const sortedTransactions = [...transactions].sort((a, b) =>
-      a.category.localeCompare(b.category)
-    );
-    setTransactions(sortedTransactions);
-  };
-
-  const sortTransactionsByDescriptionHandler = () => {
-    const sortedTransactions = [...transactions].sort((a, b) =>
-      a.description.localeCompare(b.description)
-    );
-    setTransactions(sortedTransactions);
-  };
-
-
-
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
         <h2>The Royal Bank of Flatiron</h2>
       </div>
-      <AccountContainer
-        transactions={transactions}
-        changeHandler={onChangeHandler}
-        submitHandler={onSubmitHandler}
-        searchHandler={onSearchHandler}
-        deleteHandler={onDeleteHandler}
-        sortByCategory={sortTransactionsByCategoryHandler}
-        sortByDescription={sortTransactionsByDescriptionHandler}
-        formData={formData}
-      />
+      <AccountContainer />
     </div>
   );
 }
